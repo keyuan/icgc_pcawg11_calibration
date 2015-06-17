31918a46176661fa7e5f7480c7461df02fbd2f44
Ke Yuan <ke.yuan.09@gmail.com>
2015-06-17 15:19:47 +0100

    fix a bug when loading vanloo_wedge results, replace the old peifer parser with a faster version

--- Parser.R
+++ Parser.R
@@ -1,3 +1,6 @@
+require(dplyr)
+require(tidyr)
+
 ####################################################################
 # 1_purity_ploidy
 ####################################################################
@@ -41,32 +44,84 @@ parse.dp.input = function(infile) {
 
 #' Parse a mutation assignment file
 parse.mut.assignments = function(infile) {
-  d = read.table(infile, header=T, stringsAsFactors=F)
+  if ("gz" %in% infile) {
+    d = read.delim(infile, stringsAsFactors=F)
+  } else {
+    d = read.table(infile, header=T, stringsAsFactors=F)
+  }
+  
   d = na.omit(d)
   d[,1] = as.character(d[,1])
   d = d[order(d[,1], d[,2]),]
+  if (ncol(d) == 3) {
+    d = filter(d, d[,3] !=0 )
+  } else {
+    d = filter(d, rowSums(d[,3:ncol(d)])!=0)
+  }
   return(d)
 }
 
-parse.mut.assignments.peifer = function(assignment_file, clusters_file) {
-  d = read.table(assignment_file, header=T, stringsAsFactors=F)
-  clust = read.table(clusters_file, header=T)
-  no.clust = nrow(clust)
-  
-  output = as.data.frame(matrix(0, nrow=nrow(d), ncol=2+no.clust))
-  for (i in 1:nrow(d)) {
-    # Save chromosome and position
-    output[i,1] = d[i,2]
-    output[i,2] = d[i,3]
-    # Set prob of assigning this mut to the cluster mentioned in the input to 2
-    # Adding 1 to cluster id as numbering starts at 0
-    # Adding another 2 to skip the chromosome and position columns
-    output[i,d[i,8]+1+2] = 1
+#' Parse a SV mutation assignment file
+parse.mut.assignments.markowetz_sv = function(infile) {
+  if ("gz" %in% infile) {
+    d = read.delim(infile, stringsAsFactors=F)
+  } else {
+    d = read.table(infile, header=T, stringsAsFactors=F)
   }
-  # Strip off the chr
-  output[,1] = gsub("chr", "", output[,1])
-  return(output)
+  
+  d = na.omit(d)
+  d[,1] = as.character(d[,1])
+  d = d[order(d[,1], d[,2]),]
+  if (ncol(d) == 5) {
+    d = filter(d, d[,5] !=0 )
+  } else {
+    d = filter(d, rowSums(d[,5:ncol(d)])!=0)
+  }
+  return(d)
 }
+
+
+# ke: too slow
+# parse.mut.assignments.peifer = function(assignment_file, clusters_file) {
+#   d = read.table(assignment_file, header=T, stringsAsFactors=F)
+#   clust = read.table(clusters_file, header=T)
+#   no.clust = nrow(clust)
+#   
+#   output = as.data.frame(matrix(0, nrow=nrow(d), ncol=2+no.clust))
+#   for (i in 1:nrow(d)) {
+#     # Save chromosome and position
+#     output[i,1] = d[i,2]
+#     output[i,2] = d[i,3]
+#     # Set prob of assigning this mut to the cluster mentioned in the input to 2
+#     # Adding 1 to cluster id as numbering starts at 0
+#     # Adding another 2 to skip the chromosome and position columns
+#     output[i,d[i,8]+1+2] = 1
+#   }
+#   # Strip off the chr
+#   output[,1] = gsub("chr", "", output[,1])
+#   return(output)
+# }
+
+# ke: solution based on tidyr and dplyr
+parse.mut.assignments.peifer <- function(assignment_file) {
+  d = read.table(assignment_file, header=T, stringsAsFactors=F)
+  tt = spread(d, Cluster_Id, Mut_ID)
+  tt = mutate(tt, Chr = gsub("chr", "", Chr))
+  tt = tt[, -3:-8 ]
+  tt[,-1:-2] = as.numeric(!is.na(tt[,-1:-2]))
+  return(tt)
+}
+
+#' Parse a cluster certainty file
+parse.clus.certainty = function(infile) {
+  if ("gz" %in% infile) {
+    d = read.delim(infile, stringsAsFactors=F)
+  } else {
+    d = read.delim(infile, header=T, stringsAsFactors=F)
+  }
+  return(d)
+}
+
 
 ####################################################################
 # 3_tree_structures
